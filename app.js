bc1b0d17f24382f12387bc0df82e21bc34dd57fe
Dima-BR <rawashdehdima@gmail.com>
2020-09-29 12:32:18 +0300

    add index&Css

---
+++ app.js
@@ -0,0 +1,25 @@
+var character = document.getElementById('character');
+var interval;
+var both = 0; 
+function moveLeft(){
+    var left= parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left + 2 + "px";
+}
+
+function moveRight(){
+    var left= parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left + 2 + "px";
+}
+
+document.addEventListener("keydown",event => {
+    if(event.key === "arrow-left"){
+        interval=setInterval(moveLeft,1);
+    }
+    if(event.key === "arrow-right"){
+        interval=setInterval(moveRight,1);
+    }
+});
+
+document.addEventListener("keyup", event =>{
+    clearInterval(interval);
+});
